9b302316c98f20c23b03fc8e290d9fdab366fbd8
Anant gupta <anantgupta@gmail.com>
2018-09-10 00:42:55 -0500

    get_object_or_404 func added; dynamic url routing implemented.

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -15,11 +15,13 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create!! </h1>")
 
-def post_detail(request):
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
 	context = {
-		"title": "Detail"
+		"title": "Detail",
+		"instance" : instance
 	}
-	return render(request, "index.html", context)
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
 	queryset = Post.objects.all()
