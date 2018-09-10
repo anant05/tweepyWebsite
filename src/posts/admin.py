2ddb8c632e0d86801317e15ebfc69667a00158f5
Anant gupta <anantgupta@gmail.com>
2018-09-09 22:48:10 -0500

    PostModelAdmin: Customize Admin
    PostModelAdmin: Customize Admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-
 # Register your models here.
 from .models import Post #relative import from post's model
 
-admin.site.register(Post)
+#documentation : model Admin
+
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ["title", "timestamp", "updated"]
+	list_display_links = ["updated"]
+	list_editable = ["title"]
+	list_filter = ["timestamp", "updated"]
+	search_fields = ["title", "content"]
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostModelAdmin)
+
 
