8c4fbd306a42034fae70f9ca53de36f092186eb5
Anant gupta <anantgupta@Anants-MBP.frontierlocal.net>
2018-09-09 22:31:40 -0500

    model to admin: post model included in admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post #relative import from post's model
+
+admin.site.register(Post)
+
